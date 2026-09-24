85d8ef169b708b2988fd4454bc76fa27246f9cca
Dest1n640 <realmadridfunca@gmail.com>
2026-09-24 15:26:15 +0800

    fix RecursionError

--- Src/Core/named_entity.py
+++ Src/Core/named_entity.py
@@ -7,14 +7,14 @@ class named_entity(abstract_model):
 
   def __init__(self, name: str) -> None:
     """Конструктор с именем для сущности"""
-    super().__init__
+    super().__init__()
     if name is not None and isinstance(name, str) and len(name) > 0:
-      self.name = name
+      self.__name = name
 
   @property
   def name(self) -> str:
     """Возвращаем имя сущности"""
-    return self.name
+    return self.__name
 
   @name.setter
   def name(self, new_name: str):
@@ -24,5 +24,5 @@ class named_entity(abstract_model):
     :new_name: новое имя сущности
     """
     if len(new_name) <= 0 or new_name is None or isinstance(new_name, str):
-      arguments_exception("Setter_error")
+      raise arguments_exception("Setter_error")
     self.name = new_name
